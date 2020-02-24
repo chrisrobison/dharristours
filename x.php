4647e64a67f3396d01aa8de45edd391b45491676
Christopher Robison <cdr@netoasis.net>
2020-02-24 02:06:02 -0800

    Updates
    Former-commit-id: 44d5d7ed697de0a32c201bc63407fd216c5da586

--- x.php
+++
@@ -1,9 +0,0 @@
-<?php
-   require("lib/boss_class.php");
-   $boss = new boss("sanrio.simpsf.com");
-
-   $out = $boss->getObjectRelated("HelpDesk", 589);
-
-   print_r($out);
-
-?>
--- xx.php
+++
@@ -1,9 +0,0 @@
-<?php
-   $x = "This is my #replace# #type#";
-   $in['replace'] = "awesome";
-   $in['type'] = "string";
-
-   $out = preg_replace("/\#([^\#]*)\#/e", "\$in[$1]", $x);
-
-   print $out;
-?>
