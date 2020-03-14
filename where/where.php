bfc9111441c2b34d89fdd681008a3816f706a5f7
Christopher Robison <cdr@netoasis.net>
2020-03-14 08:25:20 -0700

    Cleanup of file permissions

--- where/where.php
+++ where/where.php
@@ -3,18 +3,12 @@
    $in = $_REQUEST;
 
 
-   if (!$in || !$in['date']) {
-      $in['date'] = date("Y-m-d H:i:00");
-   }
-
-   if ($in['date']) {
-      $out = getWhere($in['date']);
-      header("Content-type: application/javascript");
-      print json_encode($out);
-   }
+   $out = getWhere($in['date'], $in['bus']);
+   header("Content-type: application/javascript");
+   print json_encode($out);
    exit;
 
-   function getWhere($date="") {
+   function getWhere($date="", $bus="") {
       $link = mysqli_connect("localhost", "root", ")wsN5WNL%=nNd\$U6", "SS_DHarrisTours");
       /* check connection */
       if (mysqli_connect_errno()) {
@@ -22,19 +16,36 @@
           exit();
       }
       
-      $mydate = strtotime($date);
-      $earlier = date("Y-m-d H:i:00", strtotime("2 hours ago", $mydate));
-      $now = date("Y-m-d H:i:00", $mydate);
+      if ($date) {
+         $mydate = strtotime($date);
+         $earlier = date("Y-m-d H:i:00", strtotime("2 hours ago", $mydate));
+         $now = date("Y-m-d H:i:00", $mydate);
 
-      $sql = "SELECT * from WebfleetBus where Created>='".$earlier."' and Created<='".$now."' order by LastModified desc limit 60";
+         $sql = "SELECT * from WebfleetBus where Created>='".$earlier."' and Created<='".$now."' order by Created desc limit 120";
+      } else { 
+         $sql = "SELECT * from WebfleetBus order by Created desc limit 120";
+      }
       $results = mysqli_query($link, $sql);
-      
+      $out = array();      
       if ($results) {
-         $out = $results->fetch_assoc();
+         while ( $row = $results->fetch_assoc()) {
 
-         if ($out) {
-            $out['PickupTime'] = date("g:ia", strtotime($out['PickupTime']));
-            $out['DropOffTime'] = date("g:ia", strtotime($out['DropOffTime']));
+            if ($row) {
+
+               $json = json_decode($row['JSON']);
+
+               if ($bus) {
+                  foreach ($json as $idx=>$obj) {
+                     if ($obj->objectno == $bus) {
+                        $out[$row['LastModified']] = $obj;
+                     }
+                  }
+               } else {
+                  $out[$row['LastModified']] = $json;
+               }
+               //$out['PickupTime'] = date("g:ia", strtotime($out['PickupTime']));
+               //$out['DropOffTime'] = date("g:ia", strtotime($out['DropOffTime']));
+            } 
          } 
       }
 
