6aa4b7a4381c235b997da87d4c8f9e6805697626
Christopher Robison <cdr@netoasis.net>
2020-04-11 06:09:28 -0700

    Addition of DVIR Report tool and Daily Driver Log tool
    Former-commit-id: 1d42caf8fd683f8ff2b647ca2f6321afa4393494

--- tools/dlog/api.php
+++ tools/dlog/api.php
@@ -8,42 +8,31 @@
        printf("Connect failed: %s\n", mysqli_connect_error());
        exit();
    }
+   
+   if ($in['type']) {
+      switch($in['type']) {
+         case "events":
+            $out = getEvents($link, $in);
+            break;
+         case "event":
+            $out = getEvent($link, $in['id']);
+            break;
+         case "drivers":
+            $out = getDrivers($link, $in);
+            break;
+         case "jobs":
+            $out = getJobs($link, $in);
+            break;
+         case "resources":
+            $out = getResources($link, $in);
+            break;
+      }
 
-   if (!$in['type']) {
-      $in['type'] = "resources";
+      if ($out) {
+         header("Content-type: application/json");
+         print json_encode($out);
+      }
    }
-   $out = array();
-
-   switch($in['type']) {
-      case "events":
-         $out = getEvents($link, $in);
-         break;
-      case "event":
-      case "job":
-         $out = getEvent($link, $in['id']);
-         break;
-      case "drivers":
-         $out = getDrivers($link, $in);
-         break;
-      case "newDamage":
-         $out = newDamage($link, $in);
-         break;
-      case "updateDamage":
-         $out = updateDamage($link, $in);
-         break;
-      case "damages":
-         $out = getDamages($link, $in);
-         break;
-      case "jobs":
-         $out = getJobs($link, $in);
-         break;
-      case "resources":
-      case "buses":
-         $out = getResources($link, $in);
-         break;
-   }
-   header("Content-type: application/json");
-   print json_encode($out);
 
    function getEvents($link, $in) {
       $colors = array('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000');
@@ -121,8 +110,6 @@
    }
 
    function getJobs($link, $in) {
-      $colors = array('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000');
-      
       $out = array(); $cnt = 0;
    
       if ($in['jobdate']) {
@@ -131,27 +118,18 @@
          $now = date("Y-m-d");
       }
       $in = $_REQUEST;
-      $threedays = date("Y-m-d", strtotime("+3 days"));
-      $yesterday = date("Y-m-d", strtotime($in['start']));
-      $first = date("Y-m-d", strtotime($in['start']));
-      $last = date("Y-m-d", strtotime($in['end']));
-      $sql = "SELECT JobID, Job.Job as Job, Job.JobDate as JobDate, PickupTime, DropOffTime, PickupLocation, DropOffLocation, NumberOfItems, Job.BusID as BusID, SpecialInstructions FROM Job where JobDate>='{$first}' AND JobDate<='{$last}' AND JobCancelled=0";
+      $sql = "SELECT JobID, Job.Job as Job, Job.JobDate as JobDate, PickupTime, DropOffTime, PickupLocation, DropOffLocation, NumberOfItems, Job.BusID as BusID, SpecialInstructions FROM Job where JobDate='{$now}' AND JobCancelled=0 AND EmployeeID=".$in['EmployeeID'];
       $results = mysqli_query($link, $sql);
       
       while ($row = $results->fetch_assoc()) {
          $obj = new stdClass();
          $obj->id = $row['JobID'];
          $obj->title = $row['Job'];
+         $obj->pickup = $row['PickupLocation'];
+         $obj->dropoff = $row['DropOffLocation'];
          $obj->start = date("c", strtotime($row['JobDate'].' '.$row['PickupTime']));
          $obj->end = date("c", strtotime($row['JobDate'].' '.$row['DropOffTime']));
-         $obj->resourceId = $row['BusNumber'];
 
-	 $obj->color = $colors[$cnt];
-         $obj->url = "/grid/view.php?rsc=Job&pid=335&id={$row['JobID']}";
-         $cnt++;
-	 if ($cnt > count($colors)) {
-	 	$cnt = 0;
-	}
          array_push($out, $obj);
       }
 
@@ -182,78 +160,6 @@
          }
          
          array_push($out, $obj);
-      }
-
-      return $out;
-   }
-   
-   function getFields($link, $tbl) {
-
-      $results = mysqli_query($link, "SHOW FIELDS FROM $tbl");
-      $out = array();
-      while ($row = $results->fetch_assoc()) {
-         array_push($out, $row['Field']);
-      }
-      return $out;
-   }
-
-   function updateDamage($link, $in) {
-      $out = array(); $cnt = 0;
-      $fields = getFields($link, 'Damage');
-      $results = mysqli_query($link, "SELECT * FROM Damage where DamageID='" . $in['DamageID']."'");
-      $orig = $results->fetch_assoc();
-
-      $fcnt = count($fields);
-      $upd = array();
-      for ($i=0; $i<$fcnt; $i++) {
-         $field = $fields[$i];
-         if ($in[$field] && ($orig[$field] != $in[$field])) {
-            $upd[] = "$field = '" .$in[$field]."'";
-         }
-      }
-
-      $sql = "UPDATE Damage set ".implode(", ", $upd)." where DamageID='" . $in['DamageID'] . "'";
-      $results = mysqli_query($link, $sql);
-
-      $out = new stdClass();
-      $out->results = $results;
-      $out->sql = $sql;
-
-      return $out;
-   }
-   
-   function newDamage($link, $in) {
-      $values = json_decode($in['json']);
-      $keys = array();
-      $vals = array();
-      $fields = getFields($link, "Damage");
-      foreach ($fields as $idx=>$field) {
-         if ($values->$field) {
-            $keys[] = $field;
-            $vals[] = $values->$field;
-         }
-      }
-
-      $sql = "INSERT INTO Damage (" . implode(",", $keys) .") VALUES ('" .implode("','", $vals)."')";
-      $results = mysqli_query($link, $sql);
-
-      $out = new stdClass();
-      $out->results = $results;
-      $out->sql = $sql;
-
-      return $out;
-   }
-
-   function getDamages($link, $in) {
-      $out = array(); $cnt = 0;
-
-      if ($in['BusID']) {
-         $xtra = " AND BusID='" . $in['BusID'] . "'";
-      }
-      $results = mysqli_query($link, "SELECT * from Damage WHERE Status!='complete'" . $xtra);
-      
-      while ($row = $results->fetch_assoc()) {
-         array_push($out, $row);
       }
 
       return $out;
