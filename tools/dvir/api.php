3e78e09dfafa67b556939fed4edc5bbb79ba1003
Christopher Robison <cdr@netoasis.net>
2020-04-19 10:19:18 -0700

    Fixed permissions on files  More work on driver logs and DVIR reports
    Former-commit-id: 08b6bf9f551a74be3313b75689a28ae230127d57

--- tools/dvir/api.php
+++ tools/dvir/api.php
@@ -162,7 +162,7 @@
       $colors = array('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000');
       
       $out = array(); $cnt = 0;
-      $results = mysqli_query($link, "SELECT * FROM Bus WHERE Active=1");
+      $results = mysqli_query($link, "SELECT * FROM Bus WHERE Capacity>0 and Active=1 order by BusNumber");
       
       while ($row = $results->fetch_assoc()) {
          $obj = new stdClass();
