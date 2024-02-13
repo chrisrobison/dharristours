8ff0b8d85ad93b18141172ace84532645bfa399c
Christopher Robison <cdr@netoasis.net>
2024-02-13 03:49:58 -0800

    Fixed spelling error

--- grid/import.php
+++ grid/import.php
@@ -12,7 +12,7 @@
       <p> - OR -</p>
       Text of Data to Import: <textarea name='importText' id='importText' style='width:25em;height:4em;'></textarea>
    </form>
-   <p class='help' style='display:none;'>To import data into '<?php print $in['rsc']; ?>' data store you may either upload a file or copy &amp; paste the text of the data to import.  The first line of the data <strong>must</strong> contain the names of the fields and those names must match those in the table.  The remaining lines contain your delimited data, one entry per line.  Valid delimiters are 'Tab', '|', and ','.  Values may optionally be quoted with double quotes ("). </p>
+   <p class='help' style='display:none;'>To import data into '<?php print $in['rsc']; ?>' data store you may either upload a file or copy &amp; paste the text of the data to import.  The first line of the data <strong>MUST</strong> contain the names of the fields and those names must match those in the table.  The remaining lines contain your delimited data, one entry per line.  Valid delimiters are 'Tab', '|', and ','.  Values may optionally be quoted with double quotes ("). </p>
 </div>
 <script>
 jQuery(function($) {
