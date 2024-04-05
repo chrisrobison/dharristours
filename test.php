20c4972f0c8ba842cfa3ecde4207971edd2a7b76
Christopher Robison <cdr@netoasis.net>
2024-04-05 12:36:58 -0700

    File cleanup

--- test.php
+++ test.php
@@ -3,11 +3,16 @@
 
    $boss = new boss("dharristours.simpsf.com");
 
-    $first = "2024-01-01";
-    $last = "2024-04-01";
+//   $first = "2024-01-01";
+//    $last = "2024-04-01";
    //$obj = $boss->getObjectRelated("Job", "JobDate>='{$first}' AND JobDate<='{$last}'");
-   $obj = $boss->getObjectRelated("Job", 33046);
-print_r($obj);
-$then = date("Y-m-d", strtotime("2 weeks"));
-print $then."\n";
+
+   $obj = new stdClass();
+   $obj->Business = ["new1"=>[ "Business"=>"Testing" ]];
+    $ids = $boss->storeObject($obj);
+
+   //$obj = $boss->getObjectRelated("Job", 33046);
+print_r($ids);
+//$then = date("Y-m-d", strtotime("2 weeks"));
+//print $then."\n";
 
