2e99e47770b646488c2346d5dbffac25e9211865
Christopher Robison <cdr@netoasis.net>
2024-05-30 16:40:19 -0700

    Cleaned up email code

--- emailurl.php
+++ emailurl.php
@@ -1,6 +1,6 @@
 <?php
    require($_SERVER['DOCUMENT_ROOT'] . '/lib/boss_class.php');
-   $boss = new boss();
+   $boss = new boss("dharristours.simpsf.com");
    $in = $_REQUEST;
 
    $in['Url'] = ($in['u']) ? $in['u'] : $in['Url'];
@@ -11,10 +11,11 @@
    $in['Date'] = date('r');
 
    if (!$in['Url']) $in['Url'] = "https://dharristours.simpsf.com/clients/dharristours/invoices/.html";
-   if (!$in['To']) $in['To'] = "patrick@simpsf.com,cdr@cdr2.com";
+   if (!$in['To']) $in['To'] = "patrick@dharristoursmail.com,cdr@cdr2.com";
    if (!$in['Subject']) $in['Subject'] = $url;
-   if (!$in['From']) $in['From'] = "support@simpsf.com";
-   if (!$in['Name']) $in['Name'] = "Simple Software System";
+   if (!$in['From']) $in['From'] = "support@dharristours.com";
+   if (!$in['Name']) $in['Name'] = "D Harris Tours";
+   $in['Bcc'] = "dharristoursrelay@gmail.com,patrick@dharristoursmail.com,cdr@cdr2.com";
 
    $tmpl = file_get_contents('apps/templates/emails/htmlmime.eml');
 
@@ -23,4 +24,18 @@
    $email = preg_replace("/#(\w+)#/e", "\$in[$1]", $tmpl);
 
    $boss->utility->sendMail($email, $in['From'], $in['Name']);
+
+    $save = new stdClass();
+    $save->Sent = new stdClass();
+    $save->Sent->new1 = new stdClass();
+    
+    $new = $save->Sent->new1;
+    $new->Sender = "support@dharristours.com";
+    $new->Message = $email;
+    $new->To = $in['To'];
+    $new->Subject = $in['Subject'];
+    $new->Url = $in['Url'];
+    
+    $boss->storeObject($save);
+
 ?>
